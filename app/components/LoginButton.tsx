07e46cd23a9a0d46d986cfe385569f94310a82d7
sushiAlii <aliasgar.laut@gmail.com>
2024-03-16 23:40:20 +0800

    updated register button as Link instead of button

--- app/components/LoginButton.tsx
+++ app/components/LoginButton.tsx
@@ -1,19 +1,21 @@
 "use client";
 
 import React from "react";
-import styles from "../page.module.css";
+import Link from "next/link";
+import styles from "@/app/page.module.css";
 
 const LoginButton = () => {
   return (
     <>
       <button
         className={styles.loginButton}
+        type="submit"
         onSubmit={() => console.log("DO LOGIN!")}
       >
         Log In
       </button>
       <span>
-        Need an account? <button>Register!</button>
+        Need an account? <Link href="/register">Register!</Link>
       </span>
     </>
   );
